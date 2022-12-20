1c9e268eead68bf4598449070e4a498659282df0
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-20 21:54:55 +0530

    Add files via upload

---
+++ Integerliteralex.java
@@ -0,0 +1,10 @@
+class Integerliteralex{
+    public static void main(String ... sachin){
+        int a =104;
+        int b= 0150;
+        int c= 0x68;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
