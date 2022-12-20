8d7cbed91e63262e80b68511f4e0d863ef34c087
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-20 21:25:34 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,11 @@
+
+public class Add {
+
+	public static void main(String[] args) {
+		int num1=3 ,num2=4;
+		int sum = num1+num2;
+		System.out.println(sum);
+		// TODO Auto-generated method stub
+
+	}
+}
