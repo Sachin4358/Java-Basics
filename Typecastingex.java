166383e5e0fe8895b76bd124c4ff20e345c6ec3b
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-21 00:31:15 +0530

    Add files via upload

---
+++ Typecastingex.java
@@ -0,0 +1,12 @@
+class Typecastingex{
+    public static void main(String ... f){
+        char a = 'h';
+        int b = a;
+        System.out.println(a);
+        System.out.println(b);
+        int c =104;
+        char d = (char) c ;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
