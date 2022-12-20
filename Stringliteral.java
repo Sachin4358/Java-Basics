f7c96739df68b4edaf0cc0a68bacb1c54c65c8b1
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-21 00:31:45 +0530

    Add files via upload

---
+++ Stringliteral.java
@@ -0,0 +1,8 @@
+class Stringliteral{
+    public static void main(String ... sir){
+        String a = "Sachin kumar singh";
+        String b = "singh saab the great";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
