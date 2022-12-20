9489b107446e9425d7b136f3ab84ff74d409aeb6
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-20 22:15:06 +0530

    Add files via upload

---
+++ Integerliteralquestion.java
@@ -0,0 +1,10 @@
+class Integerliteralquestion{
+    public static void main(String []king){
+        int a=05;
+        // int b=08; error
+        // int c=09; error
+        int d=010;
+        System.out.println(a);
+        System.out.println(d);
+    }
+}
