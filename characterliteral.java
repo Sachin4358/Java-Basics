79da75976c4a103c40e2f92474fef0012e4b78f0
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-21 00:32:18 +0530

    Add files via upload

---
+++ characterliteral.java
@@ -0,0 +1,10 @@
+class characterliteral{
+    public static void main(String ... ji){
+        char a ='h';
+        char b =104;
+        char c ='\u0068';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
