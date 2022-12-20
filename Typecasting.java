b202012e583eece1716146395d73dfdd5a3f311a
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-20 22:18:40 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,14 @@
+class Typecasting{
+    public static void main(String ... sachin){
+        int a = 10;
+        System.out.println(a);
+        double b =a;
+        System.out.println(b);
+        double c =25.3;
+        System.out.println(c);
+        int d = (int) c;
+        System.out.println(d);
+
+
+    }
+}
