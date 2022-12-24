cc2ff7eb871da77c4a6fbffd50f0f3a086911738
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-24 11:42:14 +0530

    Add files via upload

---
+++ functionex1.java
@@ -0,0 +1,26 @@
+
+public class functionex1 {
+	static void xyz() {
+	System.out.println("Sachin kumar singh");
+	System.out.println("Rajput");
+	
+	
+	}
+
+	public static void main(String[] args) {
+	System.out.println("Hello main");
+	xyz();
+	System.out.println("Sachin please call the another functon");
+	display();
+	xyz();}
+	static void display() {
+		System.out.println("Sachin singh");
+		System.out.println("Sachin is the smartest boy");
+		
+	}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+
