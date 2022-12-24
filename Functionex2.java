1cd0a080f5ef1715cccf6ca9479e98c3e16e6661
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-24 11:42:25 +0530

    Add files via upload

---
+++ Functionex2.java
@@ -0,0 +1,17 @@
+
+public class Functionex2 {
+	static void add(int x, int y) {
+		int r= x+y;
+		if(r>100) {
+			return;
+	}
+System.out.println(r);}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(50,80);
+		
+		}
+
+	}
+
+
