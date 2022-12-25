98dc52da0bedcb7092fbf1ead2c69939ba9c39ba
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-25 20:08:42 +0530

    Add files via upload

---
+++ greatest.java
@@ -0,0 +1,21 @@
+
+public class greatest {
+
+	public static void main(String[] args) {
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.println("Enter the first number");
+		int a = sc.nextInt();
+		System.out.println("Enter the second number");
+		int b = sc.nextInt();
+		if(a>b) 
+			System.out.println("a is the greater number");
+			else 
+				System.out.println("B IS THE GREATER NUMBER");
+				
+			
+		
+		// TODO Auto-generatedt method stub
+
+	}
+
+}
