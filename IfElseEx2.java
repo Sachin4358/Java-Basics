f6408a04ec9eda617cbd6ab38c9187d75923a89a
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-25 20:09:14 +0530

    Add files via upload

---
+++ IfElseEx2.java
@@ -0,0 +1,23 @@
+
+public class IfElseEx2 {
+
+	static public void main(String [] args) {
+		if(2<3) 
+			System.out.println("Hello");
+		else 
+			System.out.println("hi");
+		if(2<3) {
+			
+		System.out.println("hello");
+		System.out.println("ok");
+		}
+		
+		else{
+		System.out.println("hi ");
+		}
+		System.out.println("BYE APP");
+		
+
+	}
+
+}
