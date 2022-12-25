e9f9a54d4676fa40a93ab2db5929dac13c03ae79
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-25 20:08:59 +0530

    Add files via upload

---
+++ IfElseEx1.java
@@ -0,0 +1,16 @@
+
+public class IfElseEx1 {
+	public static void main(String...If) {
+		java.util.Scanner sc= new java.util.Scanner(System.in);
+		System.out.println("enter you age");
+		int age = sc.nextInt();
+		if(age>=13 && age<=19) 
+			System.out.println("you are teenager");
+			else {
+				System.out.println("You are not a teenager");
+			}
+		System.out.println("BYE APP");
+			
+		}
+	}
+		
