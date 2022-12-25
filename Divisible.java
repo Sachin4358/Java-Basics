ed768ad95a1ca99a7ea8a6a12ad1169a73532cb4
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-25 20:08:16 +0530

    Add files via upload

---
+++ Divisible.java
@@ -0,0 +1,13 @@
+
+public class Divisible {
+
+	public static void main(String[] args) {
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		if(num%5==0 && num%3==0) {
+			System.out.println("given number is divisible by both");}
+			else {
+				System.out.println("Enter number is not divisible by both");}
+			}
+	}
