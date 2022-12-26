8be2544e5a4ead2081c027b5d371529f99bbf16b
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-26 23:10:47 +0530

    Add files via upload

---
+++ IfElseEx5.java
@@ -0,0 +1,15 @@
+
+public class IfElseEx5 {
+
+	public static void main(String[] args) {
+		if(2>3)
+			if(2<3)
+				System.out.println("Hello");
+			else
+				System.out.println("ok");
+		System.out.println("Hi");
+		// TODO Auto-generated method stub
+
+	}
+
+}
