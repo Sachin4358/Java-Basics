26ea2ff630ee08d217733799e52d22572a2fc16a
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-26 23:11:49 +0530

    Add files via upload

---
+++ NestedIfElse2.java
@@ -0,0 +1,30 @@
+
+public class NestedIfElse2 {
+
+	public static void main(String[] args) {
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.println("Enter your first number:");
+		int a = sc.nextInt();
+		System.out.println("Enter your first number:");
+		int b = sc.nextInt();
+		System.out.println("Enter your first number:");
+		int c= sc.nextInt();
+		if(a>b) {
+			if(a>c) {
+				System.out.println("A is the greater number");}
+			else {
+				System.out.println("c s the greater number");}
+		}
+			else {
+				if(b>c){
+				System.out.println("B IS THE GREATER NUMBER");}
+			
+	
+			
+		
+			}
+		// TODO Auto-generated method stub
+
+	}
+
+}
