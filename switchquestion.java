d3f77c48793a359ac60e26f7e44b06bce13667b8
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-26 23:12:02 +0530

    Add files via upload

---
+++ switchquestion.java
@@ -0,0 +1,19 @@
+
+public class switchquestion {
+
+	public static void main(String[] args) {
+		switch(98) {
+		case 'a':
+			System.out.println("hi"); break;
+			
+		case 'b':
+			System.out.println("helllo");
+			break;
+			default:
+				System.out.println("Invalild data!");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
