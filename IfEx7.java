a64e4922eb6c911937285b8a9d06d68de0d8dba4
Sachin4358 <121093489+Sachin4358@users.noreply.github.com>
2022-12-26 23:11:18 +0530

    Add files via upload

---
+++ IfEx7.java
@@ -0,0 +1,18 @@
+
+public class IfEx7 {
+
+	public static void main(String[] args) {
+		java.util.Scanner sc= new java.util.Scanner(System.in);
+		System.out.println("Enter your age");
+		int a = sc.nextInt();
+		if(a>=13&&a<=19) {
+			System.out.println("YOU ARE A TEENAGER");
+			System.out.println("YOUR AGE:"+a);
+		}
+		System.out.println("Thnaks for using the app");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
